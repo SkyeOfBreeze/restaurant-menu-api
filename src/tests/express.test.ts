24595c3fb1f39963738adb8547a723afa985b8f8
Skye Telman <b.telman01@gmail.com>
2025-03-14 20:28:17 -0400

    Fix data.json to match spec better, and make minor changes to schema to reflect this

--- src/tests/express.test.ts
+++ src/tests/express.test.ts
@@ -30,4 +30,27 @@ describe('express server route tests', ()=>{
         expect(result.data.menu.Appetizers[0].title).toBe("Iceberg Wedge Salad with House Cured Bacon")
         expect(result.data.menu.Appetizers[0].description).toBe(undefined)
     })
+    test("Fajitas display", async()=>{
+        const result : {[key: string]: any} = await graphql({
+            schema,
+            source: `
+                {
+                    menu{
+                      Fajitas{
+                        description,
+                        items{
+                            title,
+                            price
+                        }
+                      }
+                    }
+                }`,
+            rootValue: root,
+        })
+        expect(result.data.menu.Fajitas.description).toBe("Served with red rice, black beans, grilled tomato salad, choice of corn or flour tortillas")
+        expect(result.data.menu.Fajitas.items[0].price).toBe(10.95)
+        expect(result.data.menu.Fajitas.items[1].price).toBe(10.95)
+        expect(result.data.menu.Fajitas.items[0].title).toBe("Chicken Onions, Poblano and Bell Peppers, Guacamole, Two Salsas")
+        expect(result.data.menu.Fajitas.items[1].title).toBe("Sirloin Steak, Onions, Poblano and Bell Peppers, Carrots, Onion, Guacamole, Two Salsas")
+    })
 })
